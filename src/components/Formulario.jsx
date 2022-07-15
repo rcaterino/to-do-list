9347e857b452d90faacf7f2630f0987e3eb8f611
Ricardo Fabio Caterino Polini <104625061+rcaterino@users.noreply.github.com>
2022-07-15 20:34:32 +0200

    done

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import '../estilos/formulario.css'
+
+const useInputValue = initialValue => {
+  const [value, setValue] = useState(initialValue);
+
+  return {
+    value,
+    onChange: e => setValue(e.target.value),
+    resetValue: () => setValue("")
+  };
+};
+
+export default ({ onSubmit }) => {
+  const { resetValue, ...text} = useInputValue("");
+
+  return (
+    <form 
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit(text.value);
+        resetValue();
+      }}
+    >
+      <input {...text} placeholder='Escribe una nueva tarea'></input>
+      
+    </form>
+  )
+}
+
+
